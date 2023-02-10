ab5d5d3dca7892a416263a7b27ab01cfc9d43a13
Yuki Yasoshima <yukiyasos@gmail.com>
2023-02-10 13:26:33 +0900

    App by SwiftUI (#13)

---
+++ ArchitectureExample/ArchitectureExample/ArchitectureExampleApp.swift
@@ -0,0 +1,32 @@
+//
+//  ArchitectureExampleApp.swift
+//
+
+import SwiftUI
+
+@main
+struct ArchitectureExampleApp: App {
+    let presenter: AppPresenter?
+
+    init() {
+        self.presenter = Self.makeAppPresenter()
+        self.presenter?.didFinishLaunching()
+    }
+
+    var body: some Scene {
+        WindowGroup {
+            EmptyView()
+        }
+    }
+}
+
+@MainActor
+private extension ArchitectureExampleApp {
+    static func makeAppPresenter() -> AppPresenter? {
+        if isNotTest {
+            return .init(appLifecycle: LifetimeAccessor.appLifecycle)
+        } else {
+            return nil
+        }
+    }
+}
