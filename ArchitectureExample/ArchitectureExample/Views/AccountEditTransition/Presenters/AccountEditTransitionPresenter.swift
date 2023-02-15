c58a5126f455e2fa4637c628b3e21d9f4c700813
Yuki Yasoshima <yukiyasos@gmail.com>
2023-02-15 18:09:36 +0900

    Remove isModalInPresentation

--- ArchitectureExample/ArchitectureExample/Views/AccountEditTransition/Presenters/AccountEditTransitionPresenter.swift
+++ ArchitectureExample/ArchitectureExample/Views/AccountEditTransition/Presenters/AccountEditTransitionPresenter.swift
@@ -10,20 +10,12 @@ final class AccountEditTransitionPresenter: ObservableObject {
 
     private weak var accountEditInteractor: AccountEditInteractor?
 
-    @CurrentValue private(set) var isModalInPresentation: Bool = false
     @Published private(set) var interactiveDismissDisabled: Bool = false
-
-    private var cancellables: Set<AnyCancellable> = .init()
 
     init(accountEditLifetimeId: AccountEditLifetimeId,
          accountEditInteractor: AccountEditInteractor) {
         self.accountEditLifetimeId = accountEditLifetimeId
         self.accountEditInteractor = accountEditInteractor
-
-        accountEditInteractor.$isEdited
-            .assign(to: \.value,
-                    on: self.$isModalInPresentation)
-            .store(in: &self.cancellables)
 
         accountEditInteractor.$isEdited
             .assign(to: &$interactiveDismissDisabled)
